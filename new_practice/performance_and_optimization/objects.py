1977fe936e3149943ff15cee891dc382c8526efd
pavel-malin <kurchevskijpavel@gmail.com>
2019-10-15 00:07:51 +0300

    performance and optimization/

--- new_practice/performance_and_optimization/objects.py
+++ new_practice/performance_and_optimization/objects.py
@@ -3,7 +3,20 @@ class Point(object):
         self.x = x
         self.y = y
 
-
 class Foobar(object):
     def __init__(self, x):
         self.x = x
+
+class Foobars(object):
+    __slots__ = ('x',)
+
+    def __init__(self, x):
+        self.x = x
+
+
+@profile
+def main():
+    f = [Foobar(42) for i in range(100000)] # Foobar -->> Foobars memory
+
+if __name__ == '__main__':
+    main()
